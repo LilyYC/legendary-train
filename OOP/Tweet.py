4cbc060303726cf395237cfe30a9e6b09a8591ff
Lily Chen <lily68086@gmail.com>
2020-07-19 18:51:59 +0000

    Delete Tweet.py

--- OOP/Tweet.py
+++
@@ -1,8 +0,0 @@
-"""
-Object-Oriented Programming: Twitter Example
-
-=== Module Description ===
-This module contains two sample classes Tweet and Person that we developed
-as a way to introduce the major concepts of object-oriented programming.
-"""
-
